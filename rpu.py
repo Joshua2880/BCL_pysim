31d40dcffde6ac19225d85ad0a222ea33b0db4dd
Joshua2880 <joshua.williams200000@gmail.com>
2024-05-11 14:57:31 -0400

    Converted to tensor based model

--- rpu.py
+++ rpu.py
@@ -2,6 +2,7 @@ import random
 from typing import NewType
 from enum import Enum
 from enum import IntEnum
+import numpy as np
 
 
 class ZeroByZeroDivisionError(ArithmeticError):
@@ -30,18 +31,13 @@ class RPU:
         ERROR = 1 << 3
 
     def __init__(self):
-        self.a = 0
-        self.b = 0
-        self.c = 0
-        self.d = 0
-        self.e = 0
-        self.f = 0
-        self.g = 0
-        self.h = 0
+        self.t_reg = np.array([[[0, 0],
+                                [0, 0]],
+                               [[0, 0],
+                                [0, 0]]], dtype='object')
 
-        self.x_cntr = 0
-        self.y_cntr = 0
-        self.z_cntr = 0
+        self.in_cntr = 0
+        self.out_cntr = 0
 
         self.emission_ready = False
         self.emission_val = False
@@ -50,273 +46,181 @@ class RPU:
 
         self.flags = 0
 
-    def set_registers(self, a: int, b: int, c: int, d: int, e: int, f: int, g: int, h: int):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.e = e
-        self.f = f
-        self.g = g
-        self.h = h
+    def init_tensor(self, a: int, b: int, c: int, d: int,
+                    e: int, f: int, g: int, h: int):
+        self.t_reg = np.array([[[a, b],
+                                [c, d]],
+                               [[e, f],
+                                [g, h]]], dtype='object')
 
     def init_cntrs(self):
-        self.x_cntr = 0
-        self.y_cntr = 0
-        self.z_cntr = 0
+        self.in_cntr = 0
+        self.out_cntr = 0
 
     # combinational logic in hardware version
     def update_state(self):
-        n_0 = n_1 = n_2 = n_3 = 0
-        d_0 = d_1 = d_2 = d_3 = 0
-
-        if self.op == RPU.Op.NEW:
-            n_0 = self.a
-            d_0 = self.e
-        elif self.op == RPU.Op.LFT:
-            if self.x_cntr >= 2:
-                n_0, n_1 = self.a, self.a + self.b
-                d_0, d_1 = self.e, self.e + self.f
-            elif self.x_cntr == 1:
-                n_0, n_1 = self.a, self.b
-                d_0, d_1 = self.e, self.f
-        elif self.op == RPU.Op.BLFT:
-            if self.x_cntr >= 2 and self.y_cntr >= 2:
-                n_0, n_1, n_2, n_3 = self.a, self.a + self.b, self.a + self.c, self.a + self.b + self.c + self.d
-                d_0, d_1, d_2, d_3 = self.e, self.e + self.f, self.e + self.g, self.e + self.f + self.g + self.h
-            elif self.x_cntr == 1 and self.y_cntr == 1:
-                n_0, n_1, n_2, n_3 = self.a, self.b, self.c, self.d
-                d_0, d_1, d_2, d_3 = self.e, self.f, self.g, self.h
-
-        if d_0 < 0:
-            d_0 = -d_0
-            n_0 = -n_0
-        if d_1 < 0:
-            d_1 = -d_1
-            n_1 = -n_1
-        if d_2 < 0:
-            d_2 = -d_2
-            n_2 = -n_2
-        if d_3 < 0:
-            d_3 = -d_3
-            n_3 = -n_3
-
-        if self.z_cntr != 0:
-            if n_0 < 0 and d_0 == 0:
-                n_0 = -n_0
-            if n_1 < 0 and d_1 == 0:
-                n_1 = -n_1
-            if n_2 < 0 and d_2 == 0:
-                n_2 = -n_2
-            if n_3 < 0 and d_3 == 0:
-                n_3 = -n_3
-
-        z_0 = z_1 = z_2 = z_3 = False
-        o_0 = o_1 = o_2 = o_3 = False
-
-        if self.z_cntr == 0:
-            z_0 = 0 <= n_0 and not (n_0 == 0 and d_0 == 0)
-            z_1 = 0 <= n_1 and not (n_1 == 0 and d_1 == 0)
-            z_2 = 0 <= n_2 and not (n_2 == 0 and d_2 == 0)
-            z_3 = 0 <= n_3 and not (n_3 == 0 and d_3 == 0)
-
-            o_0 = n_0 < 0
-            o_1 = n_1 < 0
-            o_2 = n_2 < 0
-            o_3 = n_3 < 0
-        elif self.z_cntr == 1:
-            z_0 = d_0 <= n_0
-            z_1 = d_1 <= n_1
-            z_2 = d_2 <= n_2
-            z_3 = d_3 <= n_3
-
-            o_0 = n_0 <= d_0
-            o_1 = n_1 <= d_1
-            o_2 = n_2 <= d_2
-            o_3 = n_3 <= d_3
-        else:
-            z_0 = n_0 <= (d_0 << 1)
-            z_1 = n_1 <= (d_1 << 1)
-            z_2 = n_2 <= (d_2 << 1)
-            z_3 = n_3 <= (d_3 << 1)
-
-            o_0 = (d_0 << 1) <= n_0
-            o_1 = (d_1 << 1) <= n_1
-            o_2 = (d_2 << 1) <= n_2
-            o_3 = (d_3 << 1) <= n_3
-
-        z_a = o_a = False
-        if self.op == RPU.Op.NEW:
-            z_a = z_0
-            o_a = o_0
-        elif self.op == RPU.Op.LFT:
-            z_a = z_0 and z_1
-            o_a = o_0 and o_1
-        elif self.op == RPU.Op.BLFT:
-            z_a = z_0 and z_1 and z_2 and z_3
-            o_a = o_0 and o_1 and o_2 and o_3
-
-        ingest_finished = (self.op == RPU.Op.NEW or
-                           self.op == RPU.Op.LFT and self.x_cntr == RPU.bcl_width or
-                           self.op == RPU.Op.BLFT and self.x_cntr == self.y_cntr == RPU.bcl_width)
-
-        self.emission_ready = (ingest_finished or
-                               self.op == RPU.Op.LFT and (z_a or o_a) or
-                               self.op == RPU.Op.BLFT and (z_a or o_a)
-                               ) and not self.z_cntr >= RPU.bcl_width
-        self.emission_val = not ingest_finished and not z_a or ingest_finished and not z_0
-
         self.flags = 0
-        if n_0 == 0 and d_0 == 0:
-            self.flags |= self.Flag.INVALID
+
+        x_interval = np.array([[1, 1], [0, 0]])
+        y_interval = np.array([[1, 1], [0, 0]])
+
+        if self.op == RPU.Op.LFT:
+            x_interval = np.array([[1, 1], [0, 1]]) if self.in_cntr >= 2 else np.array([[1, 0], [0, 1]])
+        elif self.op == RPU.Op.BLFT:
+            x_interval = np.array([[1, 1], [0, 1]]) if self.in_cntr >= 2 else np.array([[1, 0], [0, 1]])
+            y_interval = np.array([[1, 1], [0, 1]]) if self.in_cntr >= 2 else np.array([[1, 0], [0, 1]])
+
+        out_interval = np.einsum("ijl, jk", self.t_reg, x_interval)
+        out_interval = np.einsum("ijk, kl", out_interval, y_interval)
+
+        out_interval *= 2 * (out_interval[1] >= 0) - 1
+
+        zeros = np.array((2, 2), dtype=np.bool_)
+        ones = np.array((2, 2), dtype=np.bool_)
+
+        if self.out_cntr == 0:
+            zeros = np.logical_and(out_interval[0] >= 0, np.logical_or(out_interval[0] != 0, out_interval[1] != 0))
+            ones = out_interval[0] < 0
+        elif self.out_cntr == 1:
+            zeros = np.abs(out_interval[1]) <= np.abs(out_interval[0])
+            ones = np.abs(out_interval[1]) >= np.abs(out_interval[0])
+        else:
+            zeros = np.abs(out_interval[0]) <= 2 * np.abs(out_interval[1])
+            ones = np.abs(out_interval[0]) >= 2 * np.abs(out_interval[1])
+
+        zero = np.all(zeros)
+        one = np.all(ones)
+
+        ingest_finished = self.op == RPU.Op.NEW or self.in_cntr == RPU.bcl_width
+
+        self.emission_ready = (ingest_finished or zero or one) and self.out_cntr < RPU.bcl_width
+        if not ingest_finished:
+            self.emission_val = not zero
+        else:
+            if np.any(out_interval[:, 0, 0]):
+                self.emission_val = not zeros[0, 0]
+            else:
+                if zeros[0, 1] and zeros[1, 0]:
+                    self.emission_val = False
+                elif ones[0, 1] and ones[1, 0]:
+                    self.emission_val = True
+                else:
+                    if out_interval[0, 0, 0] == 0 and out_interval[1, 0, 0] == 0:
+                        self.flags |= self.Flag.INVALID
+                    raise ZeroByZeroDivisionError
 
     def new(self, n: int, d: int) -> bcl:
         if n == 0 and d == 0:
             raise ZeroByZeroDivisionError
         self.op = RPU.Op.NEW
-        self.set_registers(n, 0, 0, 0, d, 0, 0, 0)
+        self.init_tensor(n, 0, 0, 0, d, 0, 0, 0)
         self.init_cntrs()
 
         z = RPU.bcl(0)
 
         self.update_state()
         while self.emission_ready:
-            z = self.blft_emmit_z(z)
+            z = self.emmit_z(z)
+            self.out_cntr += 1
+            self.normalize()
             self.update_state()
 
         return z
 
     def frac(self, x: bcl) -> (int, int):
         self.op = RPU.Op.FRAC
-        self.set_registers(1, 0, 0, 0, 0, 0, 1, 0)
+        self.init_tensor(1, 0, 0, 0, 0, 0, 1, 0)
         self.init_cntrs()
 
-        while self.x_cntr < RPU.bcl_width:
-            x = self.blft_ingest_x(x)
-        return self.a, self.e
+        while self.in_cntr < RPU.bcl_width:
+            x = self.ingest_x(x)
+            self.in_cntr += 1
+            self.normalize()
+        return self.t_reg[0, 0, 0], self.t_reg[1, 0, 0]
 
     def blft(self, x: bcl, y: bcl, a: int, b: int, c: int, d: int, e: int, f: int, g: int, h: int) -> bcl:
         self.op = RPU.Op.BLFT
-        self.set_registers(a, b, c, d, e, f, g, h)
+        self.init_tensor(a, b, c, d, e, f, g, h)
         self.init_cntrs()
 
         z = RPU.bcl(0)
 
-        while self.z_cntr < RPU.bcl_width:
-            x = self.blft_ingest_x(x)
-            y = self.blft_ingest_y(y)
+        while self.out_cntr < RPU.bcl_width:
+            x = self.ingest_x(x)
+            y = self.ingest_y(y)
+            self.in_cntr += 1
+            self.normalize()
 
             self.update_state()
             while self.emission_ready:
-                z = self.blft_emmit_z(z)
+                z = self.emmit_z(z)
+                self.out_cntr += 1
+                self.normalize()
                 self.update_state()
 
         return z
 
-    def blft_ingest_x(self, x: bcl) -> bcl:
-        if self.x_cntr == 0:
-            if x & RPU.bcl_msb:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    -self.a, -self.b, self.c, self.d, \
-                        -self.e, -self.f, self.g, self.h
-        elif self.x_cntr == 1:
-            if x & RPU.bcl_msb:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.c, self.d, self.a, self.b, \
-                        self.g, self.h, self.e, self.f
-        elif self.x_cntr < RPU.bcl_width:
-            if x & RPU.bcl_msb:
-                if self.c & 1 or self.d & 1 or self.g & 1 or self.h & 1:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a << 1, self.b << 1, self.c, self.d, \
-                            self.e << 1, self.f << 1, self.g, self.h
-                else:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a, self.b, self.c >> 1, self.d >> 1, \
-                        self.e, self.f, self.g >> 1, self.h >> 1
-            else:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.a + self.c, self.b + self.d, self.a, self.b, \
-                        self.e + self.g, self.f + self.h, self.e, self.f
+    def ingest_x(self, x: bcl) -> bcl:
+        ingestion_val = x & RPU.bcl_msb
 
-        self.x_cntr += 1
+        op_tensor = np.array([[1, 0], [0, 1]])
+        if self.in_cntr == 0:
+            if ingestion_val:
+                op_tensor = np.array([[-1, 0], [0, 1]])
+        elif self.in_cntr == 1:
+            if ingestion_val:
+                op_tensor = np.array([[0, 1], [1, 0]])
+        elif self.in_cntr < RPU.bcl_width:
+            if ingestion_val:
+                op_tensor = np.array([[2, 0], [0, 1]])
+            else:
+                op_tensor = np.array([[1, 1], [1, 0]])
+
+        self.t_reg = np.einsum("ijl, jk", self.t_reg, op_tensor)
+
         return RPU.bcl(x << 1)
 
-    def blft_ingest_y(self, y: bcl) -> bcl:
-        if self.y_cntr == 0:
-            if y & RPU.bcl_msb:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    -self.a, self.b, -self.c, self.d, \
-                        -self.e, self.f, -self.g, self.h
-        elif self.y_cntr == 1:
-            if y & RPU.bcl_msb:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.b, self.a, self.d, self.c, \
-                        self.f, self.e, self.h, self.g
-        elif self.y_cntr < RPU.bcl_width:
-            if y & RPU.bcl_msb:
-                if self.b & 1 or self.d & 1 or self.f & 1 or self.h & 1:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a << 1, self.b, self.c << 1, self.d, \
-                            self.e << 1, self.f, self.g << 1, self.h
-                else:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a, self.b >> 1, self.c, self.d >> 1, \
-                        self.e, self.f >> 1, self.g, self.h >> 1
-            else:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.a + self.b, self.a, self.c + self.d, self.c, \
-                        self.e + self.f, self.e, self.g + self.h, self.g
+    def ingest_y(self, y: bcl) -> bcl:
+        ingestion_val = y & RPU.bcl_msb
 
-        self.y_cntr += 1
+        op_tensor = np.array([[1, 0], [0, 1]])
+        if self.in_cntr == 0:
+            if ingestion_val:
+                op_tensor = np.array([[-1, 0], [0, 1]])
+        elif self.in_cntr == 1:
+            if ingestion_val:
+                op_tensor = np.array([[0, 1], [1, 0]])
+        elif self.in_cntr < RPU.bcl_width:
+            if ingestion_val:
+                op_tensor = np.array([[2, 0], [0, 1]])
+            else:
+                op_tensor = np.array([[1, 1], [1, 0]])
+
+        self.t_reg = np.einsum("ijk, kl", self.t_reg, op_tensor)
+
         return RPU.bcl(y << 1)
 
-    def blft_emmit_z(self, z: bcl) -> bcl:
-        z <<= 1
-        z |= self.emission_val
-
-        if self.z_cntr == 0:
+    def emmit_z(self, z: bcl) -> bcl:
+        op_tensor = np.array([[1, 0], [0, 1]])
+        if self.out_cntr == 0:
             if self.emission_val:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    -self.a, -self.b, -self.c, -self.d, \
-                        self.e, self.f, self.g, self.h
-        elif self.z_cntr == 1:
+                op_tensor = np.array([[-1, 0], [0, 1]])
+        elif self.out_cntr == 1:
             if self.emission_val:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.e, self.f, self.g, self.h, \
-                        self.a, self.b, self.c, self.d
+                op_tensor = np.array([[0, 1], [1, 0]])
         else:
             if self.emission_val:
-                if self.a & 1 or self.b & 1 or self.c & 1 or self.d & 1:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a, self.b, self.c, self.d, \
-                            self.e << 1, self.f << 1, self.g << 1, self.h << 1
-                else:
-                    self.a, self.b, self.c, self.d, \
-                        self.e, self.f, self.g, self.h = \
-                        self.a >> 1, self.b >> 1, self.c >> 1, self.d >> 1, \
-                            self.e, self.f, self.g, self.h
+                op_tensor = np.array([[1, 0], [0, 2]])
             else:
-                self.a, self.b, self.c, self.d, \
-                    self.e, self.f, self.g, self.h = \
-                    self.e, self.f, self.g, self.h, \
-                        self.a - self.e, self.b - self.f, self.c - self.g, self.d - self.h
+                op_tensor = np.array([[0, 1], [1, -1]])
 
-        self.z_cntr += 1
-        return RPU.bcl(z)
+        self.t_reg = np.einsum("ij, jkl", op_tensor, self.t_reg)
+
+        return RPU.bcl((z << 1) | self.emission_val)
+
+    def normalize(self):
+        bits_set = np.bitwise_or.reduce(self.t_reg, axis=(0, 1, 2))
+        self.t_reg >>= (bits_set & -bits_set).bit_length()-1
 
     def add(self, x: bcl, y: bcl) -> bcl:
         return self.blft(x, y, 0, 1, 1, 0, 0, 0, 0, 1)
